5a9718feaff03dcfbc36b32b028c4854dff2e990
catb0y <mgn.palandri@gmail.com>
2019-02-24 23:08:12 +0100

    Solution for sentiment analysis

--- sentiments.py
+++ sentiments.py
@@ -3,8 +3,7 @@ import pandas as pd
 import re
 import os
 import requests
-from polyglot.text import Text
-
+from textblob_de import TextBlobDE as TextBlob
 
 # TODO reassign it all to big dataframe
 
@@ -15,44 +14,32 @@ from polyglot.text import Text
 # dataframe.head(100).to_pickle("data/testing_dataframe.pkl")
 dataframe = pd.read_pickle(os.path.join('data', 'testing_dataframe.pkl'))
 
-# # Tokenize data
-# german_tokenizer = nltk.data.load('tokenizers/punkt/german.pickle')
-# dataframe.text = dataframe.text.apply(lambda text_in_dataframe: german_tokenizer.tokenize(text_in_dataframe))
-
-
 # Further clean data from gibberish?
 
 
 # Sentiment analysis
+# Get sentiments with TextBlob
 
-def get_sentences(corpus):
-    text_list = []
+def get_sentiments_blob(sentence):
+    text = TextBlob(sentence)
+    return text.sentiment[0] + 1
+
+
+def get_polarity_by_newspaper(corpus):
+    sentiment_list = []
     for daily_paper in corpus.values:
-        sentence = re.split('[?.!]', " ".join(daily_paper.split()))
-        text_list.extend(sentence)
-    return text_list
+        sentiment_score = 0
+        sentence_list = re.split('[?.!]', daily_paper)
+        for sentence in sentence_list:
+            sentiment_score += get_sentiments_blob(sentence)
+        sentiment_list.append(sentiment_score/len(sentiment_list))
+    return sentiment_list
+
 
 # TODO sometimes sentences are dates, titles, etc
 
-all_sentences = get_sentences(dataframe.text)
-# }
 
-# Get sentiments with ipublia servers
-def get_sentiments(sentences):
-    response = requests.post("http://127.0.0.1:5000/predict", json={
-        "texts": sentences
-    })
-    return response.json()
-
-# get_sentiments(all_sentences)
+dataframe["polarity_by_newspaper"] = get_polarity_by_newspaper(dataframe.text)
+dataframe.to_pickle("data/dataframe_with_polarity.pkl")
 
 
-# Get sentiments with polyglot
-all_sentences_tpl = tuple(all_sentences)
-
-def get_sentiments_poly(sentences):
-    text = Text(sentences)
-    for word in text.words:
-        print(word.polarity)
-
-get_sentiments_poly(all_sentences[:10])
